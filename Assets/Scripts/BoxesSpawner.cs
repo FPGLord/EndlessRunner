d5121ba45bc6e1598a030bec96b3108df01fedcf
FPGLord <127301692+FPGLord@users.noreply.github.com>
2023-09-23 21:41:30 +0300

    SpawnBoxes Review

--- Assets/Scripts/BoxesSpawner.cs
+++ Assets/Scripts/BoxesSpawner.cs
@@ -1,31 +1,48 @@
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Assertions;
 
 public class BoxesSpawner : MonoBehaviour
 {
-    // [SerializeField] private Transform _platform;
     [SerializeField] private Transform[] _spawnPoints;
-    [SerializeField] private GameObject[] _boxes;
-
+    [SerializeField] private GameObject[] _boxPrefabs;
 
     public void SpawnBoxes()
     {
-        List<int> usedPointIndexes = new();
-        int randomPointIndex = 0;
-        
-        foreach (var box in _boxes)
+        Assert.IsTrue(_boxPrefabs.Length <= _spawnPoints.Length,
+            $" Коробок ({_boxPrefabs.Length})  больше, чем поинтов ({_spawnPoints.Length}) ");
+        ShuffleSpawnPoints();
+        for (int i = 0; i < _boxPrefabs.Length; i++)
         {
-            do
-            {
-                randomPointIndex = Random.Range(0, _spawnPoints.Length);
-            } 
-            while (usedPointIndexes.Contains(randomPointIndex));
+            _boxPrefabs[i].transform.position = _spawnPoints[i].position;
+            _boxPrefabs[i].SetActive(true);
+        }
+    }
 
-            Vector3 _spawnPointPosition = _spawnPoints[randomPointIndex].position;
-            box.transform.position = _spawnPointPosition;
-            box.SetActive(true);
-            usedPointIndexes.Add(randomPointIndex);
+    private void ShuffleSpawnPoints()
+    {
+        for (int i = 0; i < _boxPrefabs.Length; i++)
+        {
+            int randomPointIndex = Random.Range(0, _spawnPoints.Length);
+            (_spawnPoints[i], _spawnPoints[randomPointIndex]) = (_spawnPoints[randomPointIndex], _spawnPoints[i]);
         }
     }
 }
+
+
+// List<int> usedPointIndexes = new();
+// int randomPointIndex = 0;
+
+//  foreach (var box in _boxes)
+// {
+//     // do
+//     // {
+//     //     randomPointIndex = Random.Range(0, _spawnPoints.Length);
+//     // } 
+//     // while (usedPointIndexes.Contains(randomPointIndex));
+//    
+//     
+//     Vector3 _spawnPointPosition = _spawnPoints[randomPointIndex].position;
+//     box.transform.position = _spawnPointPosition;
+//     box.SetActive(true);
+//     usedPointIndexes.Add(randomPointIndex);
+// }
